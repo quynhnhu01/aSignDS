0bd58dd069f3ef6b175719885c376162e84dbbd1
thupx <xuanthu404@gmail.com>
2019-10-07 10:17:52 +0700

    Revert "adjust user interface"
    This reverts commit 39416ed14507369a12d910d8e9282d1ee2770a1f.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,31 +2,37 @@
 import styles from "./footer.module.css";
 
 export default function Footer() {
-    return (
-        <footer className={styles.footer}>
-            <div className="container-fluid">
-                <div className="row justify-content-around">
-                    <div className="col-8 col-md-5">
-                        <h5 className={styles.title}>aSignDS</h5>
-                        <p className={styles.description}>
-                            Auto Sign wiht Digital Signature and Manage your document.
-                        </p>
-                    </div>
-                    <div className="col-2">
-                        <ul className="list-unstyled">
-                            <li>
-                                <a className={styles.footerlink} href="/">Home </a>
-                            </li>
-                            <li>
-                                <a className={styles.footerlink} href="/">About</a>
-                            </li>
-                            <li>
-                                <a className={styles.footerlink} href="/">Github</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className={styles.footer}>
+      <div className="container-fluid">
+        <div className="row justify-content-around">
+          <div className="col-8 col-md-5">
+            <h5 className={styles.title}>aSignDS</h5>
+            <p className={styles.description}>
+              This is placeholder text. Your web app description goes here.
+            </p>
+          </div>
+          <div className="col-2">
+            <ul className="list-unstyled">
+              <li>
+                <a className={styles.footerlink} href="/">
+                  Example Link
+                </a>
+              </li>
+              <li>
+                <a className={styles.footerlink} href="/">
+                  Example Link
+                </a>
+              </li>
+              <li>
+                <a className={styles.footerlink} href="/">
+                  Example Link
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
