27de8ff4cecc4b016d9d33fef90a083befbd0102
tramanhk11uit <tramanh1998tls@gmail.com>
2019-10-09 08:49:24 +0700

    add : log debug

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -13,7 +13,7 @@ export default class Login extends Component {
         }
     };
     componentDidMount() {
-
+        console.log("ComponentDidMount");
     };
     handleLogin = async ({ username, password }) => {
         const response = await axios.post(CONSTANTS.ENDPOINT.LOGIN, {
