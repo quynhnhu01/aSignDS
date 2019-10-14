e74a31f4572a1de1cea941be72458c17cff2d946
thupx <xuanthu404@gmail.com>
2019-10-14 16:42:59 +0700

    add input

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useRef } from 'react';
-
+import React, { useEffect, useRef, useState } from 'react';
 export default function LoginForm(props) {
     const inputElementRef = useRef(null);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     useEffect(() => {
         inputElementRef.current.focus();
     }, []);
@@ -9,9 +10,24 @@ export default function LoginForm(props) {
         <div style={{ justifyContent: 'center', display: 'flex' }}>
             <div className="col-md-3"  >
                 <div className="form-group">
-                    <input type="text" placeholder="Username" ref={inputElementRef} className="form-control" required />
-                    <input type="password" className="form-control" placeholder="Password" required />
-                    <button type="submit" onClick={() => alert("login")} className="btn btn-primary">Login</button>
+                    <input
+                        value={username}
+                        type="text"
+                        placeholder="Username"
+                        ref={inputElementRef}
+                        className="form-control"
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+                    <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                    <button onClick={() => props.login(username, password)} className="btn btn-primary">Login</button>
                 </div>
             </div>
         </div>
