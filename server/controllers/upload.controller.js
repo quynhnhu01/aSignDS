7aa41fc0e89208b594c86f1d8d4c131a847d9159
tramanhk11uit <tramanh1998tls@gmail.com>
2019-10-22 17:23:12 +0700

    add new Contract

--- server/controllers/upload.controller.js
+++ server/controllers/upload.controller.js
@@ -1,44 +1,60 @@
-const uploadModel = require('../models/upload.model');
+const contractModel = require('../models/upload.model');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const CONSTANTS = require('../constants');
-const NodeRSA = require('node-rsa');
 const multer = require("multer");
+// const Request = require('express/lib/request');
+//const disk = multer({dest:'newContractoads/'});
 /* phần của multer */
 const diskStorage = multer.diskStorage({
-    destination: (req, file, callback) => {
-      callback(null, "uploads");
-    },
-    filename: (req, file, callback) => {
-      const filename = '${Date.now()}-AS-${file.originalname}';
-      callback(null, filename);
-    }
-  });
+  destination: (req, file, callback) => {
+    // Định nghĩa nơi file newContractoad sẽ được lưu lại
+    callback(null, "newContractoads");
+    //res.json({'message': 'File newContractoaded successfully'});
+  },
+  filename: (req, file, callback) => {
+    callback(null, file.originalname);
+  }
+});
 
-const uploadFile = multer({storage: diskStorage}).single("file");
+const upFile = multer({ storage: diskStorage }).single("file");
+
 /*------- phần của multer ------*/
 
 async function getContract(req, res) {
-    // lấy form để show hợp đồng
+  // lấy form để show hợp đồng
 };
 
 async function postContract(req, res) {
+  upFile(req, res, async function (err) {
+    //const newContract;
+    console.log(req.file);
     
-    try {
-        //res.json({'message': 'File uploaded successfully'});
-        // const contractUpload = req.body;
-        // contractUpload.createdAt = Date.now();
-        // contractUpload.updatedAt = Date.now();
-        // contractUpload.contractUrl = path.join('${__dirname}/uploads/${req.file.filename}');
-        uploadFile((req, res) => {
-            res.json({ message: "success", error: success, success: true });
-        });
-        
-    } catch (error) {
-        res.json({ message: "Error when trying to upload", error: error, success: false })
+    console.log(req.file.filename);
+
+    if (err) {
+      res.status(400).send('fail saving image');
+    } else {
+      const newContract = {};
+      newContract.nameContract = "hop dong xxx";
+      newContract.owner = req.username;
+      newContract.parner = "";
+      newContract.createdAt = Date.now();
+      newContract.updatedAt = Date.now();
+      newContract.contractUrl = __dirname + req.file.filename;
+      console.log(newContract);
+
+      const result = await contractModel.create(newContract);
+      if (result) return res.json({ message: "Create user success", result, error: null, success: true });
+      console.log(result);
+
+      // newContract.contractUrl = req.file.filename;
+      // res.send(req.file.filename);
     }
+
+  });
 };
 
 module.exports = {
-    postContract: postContract,
+  postContract: postContract,
 }
