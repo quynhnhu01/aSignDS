0cc933722aa1237f76b8f263ca018aca13be635d
thupx <xuanthu404@gmail.com>
2019-11-06 14:51:22 +0700

    update package

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,11 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import AppTest from './appTest'
 ReactDOM.render(
     <BrowserRouter>
         <App />
+        {/* <AppTest /> */}
     </BrowserRouter>,
     document.getElementById("root")
 );
