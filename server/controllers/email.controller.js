abe200e7d0487d7d580dbc80659dc34b2a3c9efb
thupx <xuanthu404@gmail.com>
2019-11-24 16:54:03 +0700

    add gmail service, now, invite using contract id & email partner

--- server/controllers/email.controller.js
+++ server/controllers/email.controller.js
@@ -1,64 +1,70 @@
 const nodemailer = require('nodemailer');
 const userModel = require('../models/user.model');
 const contractModel = require('../models/contract.model');
+const verifyModel = require('../models/verify.model');
+
 const Validator = require('class-validator').Validator;
 const validator = new Validator();
 async function sendMail(req, res) {
     try {
         const username = req.user.username;
         const partnerEmail = req.params.partner;
+        const contractId = req.params.id;
         if (!validator.isEmail(partnerEmail)) throw new Error('Email is not valid');
         const owner = await userModel.findOne({ username: username }).select('-password');
         console.log("owner", owner);
         if (!owner) throw new Error('Not found user');
         const newPartner = await userModel.findOne({ email: partnerEmail }).select('-password');
         if (!newPartner) throw new Error('Partner email is not registered');
+
+        const contract = await contractModel.findById(contractId);
+        if (contract.owner != owner._id.toString()) return res.json({ message: 'You don\'t have permission', success: false });
+        if (contract.partner.find(partnerId => partnerId == newPartner.id)) return res.json({ message: 'This partner already joined', success: false });
+
+        //create verify code 6 number
+        const code = Math.floor(100000 + Math.random() * 900000);
         const sender = await nodemailer.createTestAccount();
         const transporter = nodemailer.createTransport({
             // host: 'aspmx.l.google.com',
-            host: 'smtp.ethereal.email',
-            port: 587,
-            secure: false,
+            // host: 'smtp.ethereal.email',
+            // port: 587,
+            // secure: false,
             auth: {
-                user: sender.user,
-                pass: sender.pass,
-            }
+                user: 'pxtpxt998@gmail.com',
+                pass: 'Aa01678441221',
+            },
+            service: 'gmail',
         });
         const info = await transporter.sendMail({
-            from: owner.email,
+            from: 'noreply@asignds.com',
             to: partnerEmail,
             subject: "Invitation to new contract",
-            text: `<h1>Hello ${partnerEmail}, </h1>
-                <p> You are invited to join a new contract with ${owner.email}</p>
-                <br />
-                <p> Check it out : link http </p>
-                ------------------------------------------------------------------
+            html: `<h1>Hello ${newPartner.username}, </h1>
+                <p> You are invited to join a new contract with ${owner.username}</p>
+                <p> Use this code to verify your invitation : <b> ${code} </b> </p>
+                <p> Go to <a href="http://localhost:3001/verify"> this page</a> and verify your invitation</p>
+                -------------------------------------------------------------------------------------------- </br>
                 <b> This email is automatically, please do not reply</b>
             `
         });
-        console.log("result send mail", info);
-        `result send mail {
-            accepted: [ 'xuanthu404@gmail.com' ],
-            rejected: [],
-            envelopeTime: 916,
-            messageTime: 618,
-            messageSize: 626,
-            response: '250 Accepted [STATUS=new MSGID=XdIe14o5dlrp4TBCXdIe21H7a1sj4A0TAAAAAZaseUEGLbsk8gFeA.CZMFA]',
-            envelope: { from: '', to: [ 'xuanthu404@gmail.com' ] },
-            messageId: '<7c87c3e0-ffaa-a416-9f38-b597e461226b@PXT>'
-          }`
-        if (info) {
+        if (info.response) {
             console.log("update contract");
-            const updatedContract = await contractModel.findOneAndUpdate({ owner: owner.id }, { $addToSet: { partner: newPartner.id } }, { new: true })
-            if (updatedContract)
-                return res.json({ 'result': updatedContract, 'message': nodemailer.getTestMessageUrl(info) });
-            else throw new Error('Could not updated contract');
+            const newVerifyCode = await verifyModel.create({
+                code: code,
+                partner: newPartner.id,
+                contract: contract.id,
+            });
+            if (newVerifyCode) {
+                console.log("Create new verify code");
+                return res.json({ message: 'Invited, waiting for verification from your partner', success: true });
+            }
+
         }
         else throw new Error('Could not send email')
     } catch (error) {
         console.log("error", error);
 
-        res.send(error.message);
+        res.send({ message: error.message, error: error.name, success: false });
     }
 }
 
--- server/routes/email.js
+++ server/routes/email.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { emailController } = require("../controllers");
 const { checkAuth } = require("../middlewares");
 const routeEmail = express.Router();
-routeEmail.get('/invite/:partner', checkAuth, emailController.sendMail);
+routeEmail.get('/invite/:id&:partner', checkAuth, emailController.sendMail);
 
 module.exports = routeEmail;
