2f7b798988d540e3c39e0fc37997c2a7b35dccf7
ndinhtai <16521051@gm.uit.edu.vn>
2019-12-03 20:22:56 +0700

    - Update-show-notification

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -4,30 +4,52 @@ import { useHistory } from "react-router-dom";
 import styles from "./navbar.module.css";
 import If from "../../helpers/If";
 import { useAuthContext } from "../../contexts/auth.context";
-const Login = () => <Link className="nav-item nav-link active" to="/login">Login</Link>
-const Register = () => <Link className="nav-item nav-link active" to="/register">Register</Link>
+import Notification from "../Notification/index";
+const Login = () => (
+    <Link className="nav-item nav-link active" to="/login">
+        Login
+    </Link>
+);
+const Register = () => (
+    <Link className="nav-item nav-link active" to="/register">
+        Register
+    </Link>
+);
 export const Logout = () => {
     const { onLogout } = useAuthContext();
     const history = useHistory();
     const handleClick = () => {
-        history.push('/');
+        history.push("/");
         onLogout();
-    }
+    };
     return (
-        <button className="btn btn-primary" onClick={() => handleClick()}>Logout</button>
-    )
-}
-const Upload = () => <Link className="nav-item nav-link active" to="/upload">Upload</Link>
-const Profile = (props) => {
+        <button className="btn btn-primary" onClick={() => handleClick()}>
+            Logout
+        </button>
+    );
+};
+const Upload = () => (
+    <Link className="nav-item nav-link active" to="/upload">
+        Upload
+    </Link>
+);
+
+const Profile = props => {
     return (
-        <div style={{ display: 'flex' }}>
-            <Link className="nav-item nav-link active"
-                to="/profile"
-            >{props.username}</Link>
-            <img src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200&r=pg&d=404' width="40" height="40" className="rounded-circle" />
+        <div style={{ display: "flex" }}>
+            <Link className="nav-item nav-link active" to="/profile">
+                {props.username}
+            </Link>
+            <img
+                src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200&r=pg&d=404"
+                width="40"
+                height="40"
+                className="rounded-circle"
+                alt=""
+            />
         </div>
-    )
-}
+    );
+};
 
 export default function NavBar() {
     const { user } = useAuthContext();
@@ -37,21 +59,36 @@ export default function NavBar() {
                 <a href="#mainContent">Skip to Main Content</a>
             </div>
             <nav className="navbar navbar-dark bg-dark navbar-expand-sm border-bottom justify-content-between ">
-                <Link className="navbar-brand" to="/" style={{ fontWeight: 'bold' }}>
-                    <img src="logo.png" alt="logo" width="162px" height="54px" />
+                <Link
+                    className="navbar-brand"
+                    to="/"
+                    style={{ fontWeight: "bold" }}
+                >
+                    <img
+                        src="logo.png"
+                        alt="logo"
+                        width="162px"
+                        height="54px"
+                    />
                 </Link>
                 <div className="navbar-nav">
-                    <Link className="nav-item nav-link active" to="/">Home</Link>
+                    <Link className="nav-item nav-link active" to="/">
+                        Home
+                    </Link>
                     <If condition={!user} component={Login} />
                     <If condition={!user} component={Register} />
 
                     <If condition={user} component={Upload} />
                     <If condition={user} component={Profile} props={user} />
-                    <Link className="nav-item nav-link active" to="/about">About</Link>
-                    <If condition={user} component={Logout} />
-
+                    <Link className="nav-item nav-link active" to="/about">
+                        About
+                    </Link>
+                    <If condition={user} component={Logout} />                  
+                    <Notification />
                 </div>
             </nav>
         </React.Fragment>
     );
 }
+
+
---
+++ src/components/Notification/index.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { MDBNotification, MDBContainer } from "mdbreact";
+import "./index.scss";
+
+class Notification extends Component {
+    state = {
+        showNotification: false
+    };
+
+    handleNotification = () => {
+        const notification = this.state.showNotification;
+        if (notification) {
+            this.setState({ showNotification: false });
+        } else {
+            this.setState({ showNotification: true });
+        }
+    };
+    render() {
+        const { showNotification } = this.state;
+        return (
+            <div>
+                <img
+                    onClick={this.handleNotification}
+                    className="notification"
+                    src="https://cdn0.iconfinder.com/data/icons/ui-fill-glyphs/20/bell-512.png"
+                    alt=""
+                />
+                { showNotification ? (
+                    <MDBContainer
+                        style={{
+                            width: "auto",
+                            position: "fixed",
+                            top: "70px",
+                            right: "10px",
+                            zIndex: 9999,
+                        }}
+                        className="notificationContainer"
+                    >
+                        <MDBNotification
+                            show
+                            fade
+                            iconClassName="text-primary"
+                            title="Bootstrap"
+                            message="See? Just like this."
+                            text="just now"
+                        />
+                        <MDBNotification
+                            show
+                            fade
+                            iconClassName="text-danger"
+                            title="Bootstrap"
+                            message="Heads up, toasts will stack automatically"
+                            text="2 seconds ago"
+                        />
+                    </MDBContainer>
+                ) : null
+                }
+            </div>
+        );
+    }
+}
+
+export default Notification;
